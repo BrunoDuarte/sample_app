4e6890fe57841a6c6734b269cbc3c07f098355f7
Bruno Duarte <frank.bruno555@gmail.com>
2016-06-13 20:05:42 -0300

    one more commit that I don't remember what I did

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+  #Returns a full title in a per page bases
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
